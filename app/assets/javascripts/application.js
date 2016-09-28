7b90d94ebcd0a70a506e38d1d915e21083fc61be
Reza <reza054@gmail.com>
2016-09-28 18:33:23 -0400

    Practice Javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+greeting_doubler = function(phrase) {
+  alert(phrase + " " + phrase);
+};
+
+greeting_doubler('hello, world!');
